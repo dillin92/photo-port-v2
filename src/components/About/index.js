f05860199ef55ee03436ad27c9c3230a966909de
dillin92 <dillin970@gmail.com>
2022-04-08 22:24:57 -0400

    just for the sake of the commit

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -6,7 +6,7 @@ function About() {
   return (
 
 <section className="my-5">
-  <h1 id="about">Who am I?</h1>
+  <h1 id="about">Just for the sake of the commit</h1>
   <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
 </section>
 
